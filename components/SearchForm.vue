97b209275a65bda3f9da7959f5b89982c798935c
PMolnes <pettermolnes@icloud.com>
2023-06-11 22:47:00 +0200

    refactor: searchform to its own component

---
+++ components/SearchForm.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import RegionSelector from "~/components/RegionSelector.vue";
+
+const searchValue = ref("");
+const selectedRegion = ref("EUW");
+
+const handleSubmit = () => {
+  navigateTo(`/${selectedRegion.value}/${searchValue.value}`);
+};
+</script>
+
+<template>
+  <form
+    @submit.prevent="handleSubmit"
+    class="text-gray-700 bg-white shadow-inner shadow-gray-400 flex gap-2 items-center rounded-lg p-2"
+  >
+    <RegionSelector v-model="selectedRegion" />
+    <input
+      v-model="searchValue"
+      class="flex-1 outline-none"
+      type="text"
+      placeholder="Search for a Summoner"
+    />
+  </form>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -6,33 +6,15 @@
       class="h-full flex flex-col justify-center items-center p-10 bg-black/70"
     >
       <div class="flex flex-col gap-3">
-        <h1 class="lg:text-9xl md:text-6xl text-4xl font-extrabold">
-          TILT TRACKER
-        </h1>
-        <form
-          @submit.prevent="handleSubmit"
-          class="text-gray-700 bg-white shadow-inner shadow-gray-400 flex gap-2 items-center rounded-lg p-2"
-        >
-          <RegionSelector v-model="selectedRegion" />
-          <input
-            v-model="searchValue"
-            class="flex-1 outline-none"
-            type="text"
-            placeholder="Summoner"
-          />
-        </form>
+        <h1 class="text-dynamic font-extrabold">TILT TRACKER</h1>
+        <SearchForm />
       </div>
     </main>
   </div>
 </template>
 
-<script setup lang="ts">
-import RegionSelector from "~/components/RegionSelector.vue";
-
-const searchValue = ref("");
-const selectedRegion = ref("EUW");
-
-const handleSubmit = () => {
-  navigateTo(`/${selectedRegion.value}/${searchValue.value}`);
-};
-</script>
+<style>
+body {
+  cursor: url("~/assets/cursor.png"), auto;
+}
+</style>
