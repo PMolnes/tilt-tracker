064fc8dde64172ca65ff104f2db86ba77b0ad872
PMolnes <pettermolnes@icloud.com>
2023-06-11 14:54:32 +0200

    feat: summoner page

---
+++ pages/[region]/[summoner].vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const route = useRoute();
+
+const summoner = ref(route.params.summoner);
+const region = ref(route.params.region);
+
+const { data: summonerInfo, pending } = useLazyFetch("/api/summoner/summoner");
+</script>
+
+<template>
+  <h1>{{ summoner }}</h1>
+  <h1>{{ region }}</h1>
+  <h1 v-if="pending">Loading...</h1>
+  <h1 v-else>{{ summonerInfo }}</h1>
+</template>
