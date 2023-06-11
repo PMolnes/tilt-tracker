c5a22456c8795a64a27a9ee14acc02eb8564bd0e
PMolnes <pettermolnes@icloud.com>
2023-06-11 02:19:06 +0200

    add: listmenu for regions with headlessui

--- pages/index.vue
+++ pages/index.vue
@@ -10,12 +10,43 @@
         <div
           class="text-gray-700 bg-white shadow-inner shadow-gray-400 flex gap-2 rounded-lg p-2"
         >
-          <button
-            @click="toggleRegions"
-            class="bg-red-500 hover:bg-red-700 min-w-[4rem] text-white text-center p-2 rounded-sm"
-          >
-            EUW
-          </button>
+          <Listbox v-model="selectedRegion" as="div">
+            <ListboxButton
+              class="bg-red-500 text-white min-w-[4rem] p-2 rounded"
+              >{{ selectedRegion }}</ListboxButton
+            >
+            <Transition
+              enter-active-class="transition duration-150 ease-out"
+              enter-from-class="transform scale-y-0 origin-top opacity-0"
+              enter-to-class="transform scale-y-100 origin-top opacity-100"
+              leave-active-class="transition duration-150 ease-out"
+              leave-from-class="transform scale-y-100 origin-top opacity-100"
+              leave-to-class="transform scale-y-0 origin-top opacity-0"
+            >
+              <ListboxOptions
+                as="ul"
+                class="absolute mt-4 py-2 rounded bg-white min-w-[4rem]"
+              >
+                <ListboxOption
+                  as="template"
+                  v-for="region in regions"
+                  :key="region"
+                  :value="region"
+                  v-slot="{ active, selected }"
+                >
+                  <li
+                    :class="[
+                      selected && 'font-bold',
+                      active ? 'bg-red-500/20' : 'bg-white',
+                      'pl-3 py-2 pr-10',
+                    ]"
+                  >
+                    {{ region }}
+                  </li>
+                </ListboxOption>
+              </ListboxOptions>
+            </Transition>
+          </Listbox>
           <input
             v-model="searchValue"
             class="flex-1 outline-none"
@@ -23,17 +54,23 @@
             placeholder="Summoner"
           />
         </div>
-        <div v-if="showRegions">EUW EUNE NA</div>
       </div>
     </main>
   </div>
 </template>
 
 <script setup lang="ts">
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOptions,
+  ListboxOption,
+} from "@headlessui/vue";
+
+const regions = ["EUW", "NA", "EUNE", "SEA"];
+
 const searchValue = ref("");
 const showRegions = ref(false);
 
-const toggleRegions = () => {
-  showRegions.value = !showRegions.value;
-};
+const selectedRegion = ref(regions[0]);
 </script>
