becdaaef37654108becaa42c0bc1da3d885bf36b
PMolnes <pettermolnes@icloud.com>
2023-06-13 22:59:12 +0200

    feat: add footer component

---
+++ components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="h-48 p-4 w-full fixed bottom-0">
+    <div class="w-default h-full flex flex-col justify-center m-auto px-4">
+      <NuxtLink
+        to="/"
+        class="mr-auto text-3xl font-extrabold hover:scale-105 transition-all"
+      >
+        TT.GG
+      </NuxtLink>
+      <div>
+        <p class="text-gray-400">
+          Built by
+          <a class="underline" href="https://github.com/PMolnes" target="_"
+            >PMolnes</a
+          >. The source code is available on
+          <a class="underline" href="https://github.com/PMolnes/tilt-tracker"
+            >GitHub</a
+          >.
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
